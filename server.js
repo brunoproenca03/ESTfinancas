5c091278fe2040afb6656f02680ff6d9aa11a4d7
brunoproenca03 <142998106+brunoproenca03@users.noreply.github.com>
2024-06-28 23:42:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const CosmosClient = require('@azure/cosmos').CosmosClient;
 const config = require('./config');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para analisar o corpo da solicitação
 app.use(bodyParser.urlencoded({ extended: true }));
