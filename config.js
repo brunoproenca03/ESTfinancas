c73f97c18c889449fa762ba8480b6e0c984670d0
brunoproenca03 <brunoalexproenca@gmail.com>
2024-07-01 00:34:21 +0100

    Update index.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.endpoint = 'https://databaseprojeto.documents.azure.com:443/'
+config.endpoint = 'https://databaseprojeto.documents.azure.com/'
 config.key = 'pOmdpmDK1fnPZCvpermVndus8cZzMol4akw9IElNd0jDtrdVRdNSyxwz1kwCVWJJnUqBaTD7Q4MZACDbVMjulg=='
 
 config.database = {
